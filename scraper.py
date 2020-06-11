6736548807b349aa28086e1625b41fb2a4f3f292
Marat Bekmyrza <marat.bekmyrza@nu.edu.kz>
2020-06-11 21:06:07 +0600

    added date and time

--- scraper.py
+++ scraper.py
@@ -4,33 +4,29 @@ from selenium import webdriver
 import pymysql
 import datetime
 
-
 URL = "https://tengrinews.kz"
-TODAY = datetime.datetime.now().date()
-# yesterday = ?
+NOW = datetime.datetime.now()
+TODAY = NOW.date()
 
 conn = pymysql.connect(host="localhost", port=3306, user="root", password="sqlpassword")
 cursor = conn.cursor()
-cursor.execute("USE tengrinews;")
 
 
 def init_db():
-    cursor.execute("CREATE DATABASE tengrinews;")
+    # cursor.execute("CREATE DATABASE tengrinews;")
     cursor.execute("USE tengrinews")
     cursor.execute("DROP TABLE IF EXISTS items")
     cursor.execute("DROP TABLE IF EXISTS comments")
     sql = """CREATE TABLE items (
             id INT(11) NOT NULL,
             link VARCHAR(255) NOT NULL,
-            title TEXT NOT NULL,  
+            title TEXT NOT NULL,
+            content TEXT NOT NULL,
+            publsih_date DATE NOT NULL,
+            publish_datetime DATETIME NOT NULL,
+            parse_date DATETIME NOT NULL,  
             PRIMARY KEY (id)
             )"""
-    """
-    content TEXT NOT NULL,
-    publsih_date DATE NOT NULL,
-    publish_datetime INT(11) NOT NULL,
-    parse_date DATETIME NOT NULL,
-    """
     cursor.execute(sql)
     sql = """CREATE TABLE comments (
            id INT(11) NOT NULL,
@@ -38,11 +34,9 @@ def init_db():
            author VARCHAR(255) NOT NULL, 
            date TEXT NOT NULL,
            content TEXT NOT NULL,
+           parse_date DATETIME NOT NULL,
            PRIMARY KEY (item_id)
            )"""
-    """
-    parse_date DATETIME NOT NULL,
-    """
     cursor.execute(sql)
 
 
@@ -75,39 +69,45 @@ def scrape():
             text = ''
             for p in news_text.find_all('p'):
                 text = ''.join([text, p.get_text().strip() + ' '])
+                text.replace("'", "''")
 
             comment_items = news_soup.find('div', class_='tn-comment-list').contents
             comment_authors, comment_dates, comment_contents = [], [], []
             for comment_item in comment_items:
                 comment_authors.append(comment_item.find('span', 'tn-user-name').text)
                 comment_dates.append(comment_item.find('time').text)
-                comment_contents.append(comment_item.find('div', class_='tn-comment-item-content-text').get_text())
+                comment_text = comment_item.find('div', class_='tn-comment-item-content-text').get_text()
+                comment_contents.append(comment_text.replace("'", "''"))
 
         except:
             continue
 
-        print(title, publish_date)
-        print(link)
-        print()
-
-
+        # today and yesterday are mostly enough for the recent news
         if 'сегодня' in publish_date:
-            pub_date = TODAY
+            news_date = TODAY.strftime("%Y-%m-%d")
         elif 'вчера' in publish_date:
-            pub_date = TODAY
-            pub_date.day -= 1
+            news_date = TODAY
+            news_date.day -= 1
+            news_date.strftime("%Y-%m-%d")
 
+        news_datetime = news_date + " " + publish_date[-5:] + ":00"
+        parse_datetime = NOW.strftime("%Y-%m-%d %H:%M:%S")
 
-        sql = "INSERT INTO `items` VALUES (" + str(news_id) + ",'" + link + "','" + title + "');"
+        sql = "INSERT INTO `items` VALUES (" + str(news_id) + ",'" + link + "','" + title + "','" + text + "','" +\
+              news_date + "','" + news_datetime + "','" + parse_datetime + "');"
+        print(sql)
         cursor.execute(sql)
         for author, date, content in zip(comment_authors, comment_dates, comment_contents):
-            sql = "INSERT INTO `comments` VALUES (" + str(id) + "," + str(comment_id) + ",'" + author + "','" + date + "','" + content + "');"
+            sql = "INSERT INTO `comments` VALUES (" + str(news_id) + "," + str(comment_id) + ",'" + author + "','" + \
+                  date + "','" + content + "','" + parse_datetime + "');"
+            print(sql)
             cursor.execute(sql)
             comment_id += 1
         news_id += 1
+
     driver.close()
     conn.commit()
 
 
-# scrape()
-print(TODAY)
+init_db()
+scrape()
